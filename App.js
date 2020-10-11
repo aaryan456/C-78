2d9318436d4dd8ad28a6e7a3f0372bd3efc0481c
aaryan456 <62750261+aaryan456@users.noreply.github.com>
2020-10-11 17:41:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import LoginScreen from './screens/LoginScreen';
-import WriteScreen from './screens/WriteScreen';
-import ReadScreen from './screens/ReadScreen';
+
 
 export default class App extends React.Component {
   render() {
