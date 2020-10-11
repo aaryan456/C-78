98197b46d376f02225fe7a3147b748b7e85b4ee8
aaryan456 <62750261+aaryan456@users.noreply.github.com>
2020-10-11 18:37:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
 import LoginScreen from './screens/LoginScreen';
 
 
